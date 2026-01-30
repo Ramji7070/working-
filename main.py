a67cd55b3be5769692f7f62e5d66ea5f6a2fff10
Ramji7070 <patelramji9177@gmail.com>
2026-01-30 20:21:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -915,7 +915,7 @@ async def txt_handler(bot: Client, m: Message):
                             keys_string = ""
                     except Exception:
                         keys_string = ""
-            elif 'media-cdn.classplusapp.com' in url or "_encn" in url:
+            elif 'media-cdn.classplusapp.com' in url and "_encn" in url:
                 headers = {'host': 'api.classplusapp.com', 'x-access-token': f'{raw_text4}', 'accept-language': 'EN', 'api-version': '18', 'app-version': '1.4.73.2', 'build-number': '35', 'connection': 'Keep-Alive', 'content-type': 'application/json', 'device-details': 'Xiaomi_Redmi 7_SDK-32', 'device-id': 'c28d3cb16bbdac01', 'region': 'IN', 'user-agent': 'Mobile-Android', 'webengage-luid': '00000187-6fe4-5d41-a530-26186858be4c', 'accept-encoding': 'gzip'}
                 params = {"url": f"{url}"}
                 response = requests.get('https://api.classplusapp.com/cams/uploader/video/jw-signed-url', headers=headers, params=params)
