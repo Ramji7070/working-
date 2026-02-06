724d6e89d987451441916492db0240982ebcaf79
Ramji7070 <patelramji9177@gmail.com>
2026-02-06 16:31:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1531,7 +1531,7 @@ async def txt_handler(bot: Client, m: Message):
                     Show = f"<i><b>Video Downloading</b></i>\n<blockquote><b>{str(count).zfill(3)}) {name1}</b></blockquote>"
                     prog = await bot.send_message(channel_id, Show, disable_web_page_preview=True)
                     prog1 = await m.reply_text(Show1, disable_web_page_preview=True)
-                    res_file = helper.download_video(url, cmd, name)
+                    filename = await helper.download_video(url, name, quality)
                     filename = res_file
                     await prog1.delete(True)
                     await prog.delete(True)
