0b5df9f1f9b589a834ee5de73dff6cf5613491f3
Ramji7070 <patelramji9177@gmail.com>
2026-02-22 05:22:35 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -17,8 +17,8 @@ DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://itsgoluAPI:jrMHSipT
 MONGO_URL = DATABASE_URL  # For auth system
 
 # Owner and Admin Configuration
-OWNER_ID = int(os.environ.get("OWNER_ID", "0"))
-ADMINS = [int(x) for x in os.environ.get("ADMINS", "0").split(",")]  # Default to owner ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "8408562949"))
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "8408562949").split(",")]  # Default to owner ID
 
 # Channel Configuration
 PREMIUM_CHANNEL = ""
@@ -63,6 +63,7 @@ Please contact the admin to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
 
 
 
